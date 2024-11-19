6130ce581b99243dc2219f9186e67b0ae2351caa
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-11-19 14:32:18 -0300

    refactor: move duplicated code to a helper method

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,7 @@
+import { MissingParamError } from "../errors/missing-param-error";
+import { httpResponse } from "../protocols/http";
+
+export const badRequest = (error: Error): httpResponse => ({
+    statusCode: 400,
+    body: error 
+});
