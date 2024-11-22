8efd9c3989e1917432745c6f5ef4929cc9f88320
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-11-22 15:22:35 -0300

    refactor: move server error to a helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -93,10 +93,10 @@ describe('SignUp Controller', () => {
         expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'));
     });
 
-    test('Should call EmailValidator with correct email', () => {
+    test('Should return 400 if an invalid email is provided', () => {
         const { sut, emailValidatorStub } = makeSut();
         
-        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
+        jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
 
         const httpRequest = {
             body: {
@@ -107,8 +107,27 @@ describe('SignUp Controller', () => {
             }
         }
         
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new InvalidParamError('email'));
+    });
+
+    test('Should call EmailValidator with correct email', () => {
+        const { sut, emailValidatorStub } = makeSut();
+        
+        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
+
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        
         sut.handle(httpRequest);
-        expect(isValidSpy).toHaveBeenCalledWith('invalid_email@mail.com');
+        expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com');
     });
 
     test('Should return 500 if EmailValidator throws', () => {
@@ -134,5 +153,4 @@ describe('SignUp Controller', () => {
         expect(httpResponse.statusCode).toBe(500);
         expect(httpResponse.body).toEqual(new ServerError());
     });
-
 });
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,7 +1,12 @@
-import { MissingParamError } from "../errors/missing-param-error";
+import { ServerError } from "../errors/server-error";
 import { httpResponse } from "../protocols/http";
 
 export const badRequest = (error: Error): httpResponse => ({
     statusCode: 400,
     body: error 
+});
+
+export const serverError = (): httpResponse => ({
+    statusCode: 500,
+    body: new ServerError() 
 });
